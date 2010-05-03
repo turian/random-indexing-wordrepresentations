86b2e37a5c7155b74380fa5aa2c7176d7599b362
Joseph Turian <turian@gmail.com>
2010-05-03 17:18:35 -0400

    Can now read training sentences

--- induce.py
+++ induce.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python
 
+import sys
+import string
+from common.file import myopen
+
+def training_examples():
+    """
+    An iterator of a list of tokens (one list per line in the input data.)
+    """
+
+    HYPERPARAMETERS = common.hyperparameters.read("random-indexing")
+    from vocabulary import wordmap
+    filename = HYPERPARAMETERS["TRAIN_SENTENCES"]
+    count = 0
+    for l in myopen(filename):
+        tokens = []
+        for w in string.split(l):
+            w = string.strip(w)
+            assert wordmap.exists(w)     # Not exactly clear what to do
+                                         # if the word isn't in the vocab.
+            tokens.append(wordmap.id(w))
+        yield tokens
+
 if __name__ == "__main__":
     import common.hyperparameters, common.options
     HYPERPARAMETERS = common.hyperparameters.read("random-indexing")
     HYPERPARAMETERS, options, args, newkeystr = common.options.reparse(HYPERPARAMETERS)
+    import hyperparameters
 
     from common import myyaml
-    import sys
-    print >> sys.stderr, myyaml.dump(HYPERPARAMETERS)
+    import common.dump
+    print >> sys.stderr, myyaml.dump(common.dump.vars_seq([hyperparameters]))
+
+    for tokens in training_examples():
+        print tokens
---
+++ vocabulary.py
@@ -0,0 +1,25 @@
+"""
+Automatically load the wordmap, if available.
+"""
+
+import cPickle
+from common.file import myopen
+import sys
+
+def _wordmap_filename():
+    import common.hyperparameters, common.options
+    HYPERPARAMETERS = common.hyperparameters.read("random-indexing")
+    return HYPERPARAMETERS["VOCABULARY_IDMAP_FILE"]
+
+wordmap = None
+try:
+    wordmap = cPickle.load(myopen(_wordmap_filename()))
+    wordmap.str = wordmap.key
+except: pass
+
+def write(wordmap):
+    """
+    Write the word ID map, passed as a parameter.
+    """
+    print >> sys.stderr, "Writing word map to %s..." % _wordmap_filename()
+    cPickle.dump(wordmap, myopen(_wordmap_filename(), "w"))
